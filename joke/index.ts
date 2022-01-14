ef9413ff150551a675a576d769ab58926c852ce6
Per-Ola Gustafsson <n1175@hotmail.com>
2022-01-14 11:51:30 +0100

    Joke

---
+++ joke/index.ts
@@ -0,0 +1,26 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+interface HttpResponse {
+    status: number
+    headers?: { [name: string]: string }
+    body?: unknown    
+}
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log.info('HTTP trigger function processed a request.');
+    
+    const body = {
+        text: "No Jokes Today"
+    }
+
+    context.res = {
+        status: 200,
+        headers: {
+            "content-type": "application/json"
+        },
+        body
+    };
+
+};
+
+export default httpTrigger;
